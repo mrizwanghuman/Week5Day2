b69e9958362f63335ccab0f58938324f0c24e2e5
Muhammad Rizwan <mrizwanghuman@yahoo.com>
2017-12-20 12:08:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/admin/week5day2/homeactivity/HomeActContractor.java
@@ -0,0 +1,8 @@
+package com.example.admin.week5day2.homeactivity;
+
+/**
+ * Created by  Admin on 12/20/2017.
+ */
+
+public interface HomeActContractor {
+}
---
+++ app/src/main/java/com/example/admin/week5day2/util/BaseView.java
@@ -0,0 +1,8 @@
+package com.example.admin.week5day2.util;
+
+/**
+ * Created by  Admin on 12/20/2017.
+ */
+
+public interface BaseView {
+}
